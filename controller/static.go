5a1356ded51c2ddaeae24aa5c4b371de26fd7561
root <root@localhost.localdomain>
2016-07-14 11:01:19 -0400

    modified:   controller/index.go 	modified:   controller/static.go

--- controller/static.go
+++ controller/static.go
@@ -6,13 +6,14 @@ import (
 	"net/http"
 	pf "path/filepath"
 
+	kconst "github.com/alexxuyao/kiwiui/const"
 	"github.com/gorilla/mux"
 )
 
 func StaticController(w http.ResponseWriter, r *http.Request) {
 	fname := mux.Vars(r)["file"]
 	fmt.Println(fname)
-	fname = "../../../../../src/github.com/alexxuyao/kiwiui/web/static/" + fname
+	fname = kconst.WebPath() + "/static/" + fname
 	nf, _ := pf.Abs(fname)
 	fmt.Println(nf)
 	http.ServeFile(w, r, fname)
