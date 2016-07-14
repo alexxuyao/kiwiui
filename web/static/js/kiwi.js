de2dc54269b87df5b2aab887962962d802bd49d5
root <root@localhost.localdomain>
2016-07-14 11:21:08 -0400

    new file:   web/static/js/kiwi.js

---
+++ web/static/js/kiwi.js
@@ -0,0 +1,15 @@
+// Basic usage
+var KiwiBox = classdef({
+  constructor: function() {
+    // ...
+  },
+
+  someFunc: function() {
+    // ...
+  }
+});
+
+$(document).ready(function(){
+	var box = new KiwiBox();
+	
+});
