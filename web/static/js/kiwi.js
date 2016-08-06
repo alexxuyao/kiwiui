26657df1c36fdc20361c8ae01a8a2f45d40a8e5f
alex <xuyg@kuparts.com>
2016-08-06 18:03:10 -0400

    modified:   web/static/css/main.css 	modified:   web/static/js/kiwi.js 	modified:   web/static/js/kiwidragdrop.js 	modified:   web/template/index.html 	new file:   web/test/kwddtest.html

--- web/static/js/kiwi.js
+++ web/static/js/kiwi.js
@@ -1,418 +1,368 @@
 var constant = {
-	dropActiveLineColor : '#999900',
-	dropActiveColor : '#cccc66',
-	dropOverColor : '#aaaa33'
-}
-// 用来保证一次拖放只有一个接收者。jquery的greedy只能用在接收者有父子关系的层次结构中
-var dropUtil = {
-
-	receive : false,
-
-	// 是否已经接收了拖放事件
-	checkReceive : function(){
-		if(this.receive){
-			return true;
-		}
-
-		this.receive = true;
-
-		// fix jquery ui bug
-		$('.ui-droppable-hover').removeClass('ui-droppable-hover');
-		$('.ui-state-hover').removeClass('ui-state-hover');
-
-		return false;
-	},
-
-	// 开始拖动
-	start : function(){
-		this.receive = false;
-		this.over = false;
-	}
+    dropActiveLineColor : '#999900',
+    dropActiveColor : '#cccc66',
+    dropOverColor : '#aaaa33'
 }
 
 var util = {
 
-	// 注册忽略拖动事件
-	registerNotDrop : function(dom, cmpId){
-		dom.droppable({
-			accept: ".cmp-define",
-			greedy: true,
-			out : function(event, ui ){
-				console.debug('not-drop out:' + cmpId);
-			},
-			over : function(event, ui ){
-				console.debug('not-drop over:' + cmpId);
-			},
-			drop: function( event, ui ) {
-				if(dropUtil.checkReceive()){
-					return;
-				}
-				console.debug('drop to not drop:' + cmpId);
-				return false;
-			}
-		});
-	},
+    // 注册忽略拖动事件
+    registerNotDrop : function(dom, cmpId) {
+        dom.kwdroppable({
+            accept : "cmp-define"
+        });
+    },
 
-	idIndex : 0,
+    idIndex : 0,
 
-	// 生成一个唯一ID
-	newCmpId : function(){
-		return 'cmp-' + (++util.idIndex);
-	}
+    // 生成一个唯一ID
+    newCmpId : function() {
+        return 'cmp-' + (++util.idIndex);
+    }
 }
 
 // Basic usage
 var CmpNode = classdef({
-	id : '',
+    id : '',
 
-	cmpDefine : null,
+    cmpDefine : null,
 
-	parent : null,
+    parent : null,
 
-	context : null,
+    context : null,
 
-	positions : null, //{positonName : [CmpNode, CmpNode]}
+    positions : null, // {positonName : [CmpNode, CmpNode]}
 
-	attrs : null, //{attrName : attrValue}
+    attrs : null, // {attrName : attrValue}
 
-	constructor: function(config) {
-		// this.cmpDefine = config.cmpDefine;
-		$.extend(true, this, config);
+    constructor : function(config) {
+        // this.cmpDefine = config.cmpDefine;
+        $.extend(true, this, config);
 
-		this.positions = {};
-		this.attrs = {};
+        this.positions = {};
+        this.attrs = {};
 
-		// the attrs default value
-		for(var i in config.cmpDefine['attrs']){
-			var attr = config.cmpDefine['attrs'][i];
-			var attrName = attr['name'];
-			var val = attr['defaultValue'];
-			if(val === undefined){
-				val = '';
-			}
-			this.attrs[attrName] = val;
-		}
-	},
+        // the attrs default value
+        for ( var i in config.cmpDefine['attrs']) {
+            var attr = config.cmpDefine['attrs'][i];
+            var attrName = attr['name'];
+            var val = attr['defaultValue'];
+            if (val === undefined) {
+                val = '';
+            }
+            this.attrs[attrName] = val;
+        }
+    },
 
-	getRenderHtml : function(func){
+    getRenderHtml : function(func) {
 
-		var cloneNode = function(node){
+        var cloneNode = function(node) {
 
-			var jobj = $.extend(true, {}, {
-				id : node.id,
-				cmpDefine : node.cmpDefine,
-				positions : node.positions,
-				attrs : node.attrs
-			});
+            var jobj = $.extend(true, {}, {
+                id : node.id,
+                cmpDefine : node.cmpDefine,
+                positions : node.positions,
+                attrs : node.attrs
+            });
 
-			for (var i in jobj['positions']){
-				for(var j in jobj['positions'][i]){
-					jobj['positions'][i][j] = cloneNode(jobj['positions'][i][j]);
-				}
-			}
+            for ( var i in jobj['positions']) {
+                for ( var j in jobj['positions'][i]) {
+                    jobj['positions'][i][j] = cloneNode(jobj['positions'][i][j]);
+                }
+            }
 
-			// delete jobj['positions']
-			return jobj;
-		}
+            // delete jobj['positions']
+            return jobj;
+        }
 
-		var jobj = cloneNode(this);
+        var jobj = cloneNode(this);
 
-		var djson = JSON.stringify(jobj);
-		$.post('/getRenderHtml', djson, function(ret){
-			// alert(ret);
-			if(ret.success){
-				func(ret.data);
-			}else{
-				alert(ret.message);
-			}
+        var djson = JSON.stringify(jobj);
+        $.post('/getRenderHtml', djson, function(ret) {
+            // alert(ret);
+            if (ret.success) {
+                func(ret.data);
+            } else {
+                alert(ret.message);
+            }
 
-		});
-	},
+        });
+    },
 
-	// 添加一个子组件
-	addChild : function(child, position){
+    // 添加一个子组件
+    addChild : function(child, position) {
 
-		if(!this.positions[position]){
-			this.positions[position] = [];
-		}
+        if (!this.positions[position]) {
+            this.positions[position] = [];
+        }
 
-		this.positions[position].push(child);
+        this.positions[position].push(child);
 
-		// 找到要添加到的位置的dom元素
-		var rdom = $("[cmp-id='" + this.id + "']");
-		var pdom = rdom.find(".cmp-position[position='" + position + "']");
-		if(!pdom.length){
-			pdom = rdom;
-		}
-		pdom = pdom.eq(0);
+        // 找到要添加到的位置的dom元素
+        var rdom = $("[cmp-id='" + this.id + "']");
+        var pdom = rdom.find(".cmp-position[position='" + position + "']");
+        if (!pdom.length) {
+            pdom = rdom;
+        }
+        pdom = pdom.eq(0);
 
-		// 渲染
-		child.renderTo(pdom);
-	},
+        // 渲染
+        child.renderTo(pdom);
+    },
 
-	// 从父节点把自己删除
-	remove : function(){
+    // 从父节点把自己删除
+    remove : function() {
 
-	},
+    },
 
-	renderTo : function(dom){
-		var self = this;
-		self.getRenderHtml(function(html){
-			if(!html){
-				return;
-			}
+    renderTo : function(dom) {
+        var self = this;
+        self.getRenderHtml(function(html) {
+            if (!html) {
+                return;
+            }
 
-			self.context.processLibs(html.libs);
+            self.context.processLibs(html.libs);
 
-			var head = html.head;
-			var body = html.body;
+            var head = html.head;
+            var body = html.body;
 
-			// remove old headers
-			// $('head').find('.old-header').remove();
+            // remove old headers
+            // $('head').find('.old-header').remove();
 
-			// append new headers
-			$('head').append(head);
-			dom.append(body);
-			body = $('#' + self.id);
+            // append new headers
+            $('head').append(head);
+            dom.append(body);
+            body = $('#' + self.id);
 
-			// 绑定拖动事件
-			var bodyPositions = [body.find( ".cmp-position" )];
-			if (body.hasClass('cmp-position')){
-				bodyPositions.push(body);
-			}
+            // 绑定拖动事件
+            var bodyPositions = [ body.find(".cmp-position") ];
+            if (body.hasClass('cmp-position')) {
+                bodyPositions.push(body);
+            }
 
-			for (var i in bodyPositions){
-				bodyPositions[i].droppable({
-					accept: ".cmp-define",
-					greedy: true,
-					activate: function( event, ui ) {
-						event.stopPropagation();
-						console.debug('activate:' + self.id);
-						var target = $(event.target);
+            for ( var i in bodyPositions) {
+                bodyPositions[i].kwdroppable({
+                    accept : "cmp-define",
+                    activate : function(e) {
+                        console.debug('activate:' + self.id);
+                        var target = $(this);
 
-						var ocss = {
-							'padding-top' : target.css('padding-top'),
-							'padding-left' : target.css('padding-left'),
-							'padding-right' : target.css('padding-right'),
-							'padding-bottom' : target.css('padding-bottom'),
-							'background-color' : target.css('background-color')
-						};
+                        var ocss = {
+                            'padding-top' : target.css('padding-top'),
+                            'padding-left' : target.css('padding-left'),
+                            'padding-right' : target.css('padding-right'),
+                            'padding-bottom' : target.css('padding-bottom'),
+                            'background-color' : target.css('background-color')
+                        };
 
-						target.attr('old-style', JSON.stringify(ocss));
-						target.css('border-style', 'dashed');
+                        target.attr('old-style', JSON.stringify(ocss));
+                        target.css('border-style', 'dashed');
 
-						target.css({
-					        'padding' : 20,
-							'border-width' : '2px',
-							'border-color' : '#000000',
-							'background-color' : constant.dropActiveColor
-					    });
-					},
-					deactivate: function( event, ui ) {
-						event.stopPropagation();
-						console.debug('deactivate:' + self.id);
-						var target = $(event.target);
-						var ocss = $.parseJSON(target.attr('old-style'));
-						target.removeAttr('old-style');
+                        target.css({
+                            'padding' : 20,
+                            'border-width' : '2px',
+                            'border-color' : '#000000',
+                            'background-color' : constant.dropActiveColor
+                        });
+                    },
+                    deactivate : function(e) {
+                        console.debug('deactivate:' + self.id);
+                        var target = $(this);
+                        var ocss = {};
+                        try{
+                            ocss = $.parseJSON(target.attr('old-style'));
+                        }catch(e){
+                            console.debug(e);
+                            console.debug(target.attr('old-style'));
+                        }
+                        target.removeAttr('old-style');
 
-						target.css(ocss);
-					},
-					out : function(event, ui ){
-						event.stopPropagation();
-						console.debug('out:' + self.id);
-						var target = $(event.target);
-						target.css({
-							'background-color' : constant.dropActiveColor
-					    });
-					},
-					over : function(event, ui ){
-						event.stopPropagation();
-						console.debug('over:' + self.id);
-						var target = $(event.target);
-						target.css({
-							'background-color' : constant.dropOverColor
-					    });
-					},
-					drop: function( event, ui ) {
+                        target.css(ocss);
+                    },
+                    out : function(e) {
+                        console.debug('out:' + self.id);
+                        var target = $(this);
+                        target.css({
+                            'background-color' : constant.dropActiveColor
+                        });
+                    },
+                    over : function(event, ui) {
+                        console.debug('over:' + self.id);
+                        var target = $(this);
+                        target.css({
+                            'background-color' : constant.dropOverColor
+                        });
+                    },
+                    drop : function(e) {
+                        
+                        var targetNode = self;
+                        var cmpDefine = $.parseJSON(e.drager.find('textarea').val());
+                        var position = $(this).attr('position');
 
-						if(dropUtil.checkReceive()){
-							return;
-						}
+                        var child = new CmpNode({
+                            cmpDefine : cmpDefine,
+                            context : self.context
+                        });
 
-						console.debug('drop to cmp position:' + self.id);
+                        child.id = util.newCmpId();
+                        targetNode.addChild(child, position);
 
-						// var target = event.target;
-						var targetNode = self;
-						var cmpDefine = $.parseJSON(ui.draggable.find('textarea').val());
-						var position = $( this ).attr('position');
+                    }
+                });
+            }
 
-						var child = new CmpNode({
-							cmpDefine : cmpDefine,
-							context : self.context
-						});
+            // 绑定not-drop事件，
+            var bodyNotDrops = [ body.find(".not-drop") ];
+            if (body.hasClass('not-drop')) {
+                bodyNotDrops.push(body);
+            }
 
-						child.id = util.newCmpId();
-						targetNode.addChild(child, position);
+            for ( var i in bodyNotDrops) {
+                util.registerNotDrop(bodyNotDrops[i], self.id);
+            }
 
-					}
-				});
-			}
+        });
+    },
 
-			// 绑定not-drop事件，
-			var bodyNotDrops = [body.find( ".not-drop" )];
-			if (body.hasClass('not-drop')){
-				bodyNotDrops.push(body);
-			}
+    setAttr : function(name, value) {
 
-			for (var i in bodyNotDrops){
-				util.registerNotDrop(bodyNotDrops[i], self.id);
-			}
-
-		});
-	},
-
-	setAttr : function(name, value){
-
-	}
+    }
 });
 
 var KwContext = classdef({
 
-	cmpTreeRoot : null,
+    cmpTreeRoot : null,
 
-	libs : null,
+    libs : null,
 
-	constructor: function() {
-		this.cmpTreeRoot = null;
-		this.libs = {
-			'jquery' : '1.11'
-		};
-	},
+    constructor : function() {
+        this.cmpTreeRoot = null;
+        this.libs = {
+            'jquery' : '1.11'
+        };
+    },
 
-	// 初始化根组件
-	initRootCmp : function(cmpDefine){
-		this.cmpTreeRoot = new CmpNode({
-			cmpDefine : cmpDefine,
-			context : this
-		});
-		this.cmpTreeRoot.id = 'cmpRoot';
-		this.cmpTreeRoot.renderTo($('#rootContainer'));
-	},
+    // 初始化根组件
+    initRootCmp : function(cmpDefine) {
+        this.cmpTreeRoot = new CmpNode({
+            cmpDefine : cmpDefine,
+            context : this
+        });
+        this.cmpTreeRoot.id = 'cmpRoot';
+        this.cmpTreeRoot.renderTo($('#rootContainer'));
+    },
 
-	// 添加一个组件
-	processLibs : function(libs){
+    // 添加一个组件
+    processLibs : function(libs) {
 
-		for(var i in libs){
-			var libName = i.substring(0, i.lastIndexOf('-'));
-			var libVersion = i.substring(i.lastIndexOf('-') + 1);
+        for ( var i in libs) {
+            var libName = i.substring(0, i.lastIndexOf('-'));
+            var libVersion = i.substring(i.lastIndexOf('-') + 1);
 
-			var cVersion = this.libs[libName];
-			if(cVersion){
-				// 库已存在，比较版本
-			}else{
-				var html = '';
-				for (var j in libs[i]['js']){
-					html += '<script type="text/javascript" src="' + libs[i]['js'][j] + '"></script>';
-				}
-				for (var j in libs[i]['css']){
-					html += '<link href="' + libs[i]['css'][j] + '" rel="stylesheet" />';
-				}
+            var cVersion = this.libs[libName];
+            if (cVersion) {
+                // 库已存在，比较版本
+            } else {
+                var html = '';
+                for ( var j in libs[i]['js']) {
+                    html += '<script type="text/javascript" src="' + libs[i]['js'][j] + '"></script>';
+                }
+                for ( var j in libs[i]['css']) {
+                    html += '<link href="' + libs[i]['css'][j] + '" rel="stylesheet" />';
+                }
 
-				$('head').append(html);
+                $('head').append(html);
 
-				this.libs[libName] = libVersion;
-			}
-		}
-	}
+                this.libs[libName] = libVersion;
+            }
+        }
+    }
 });
 
 //
 var KwTreeBox = classdef({
-  constructor: function(ctx) {
-	this.initComponent();
-	this.ctx = ctx;
-  },
+    constructor : function(ctx) {
+        this.initComponent();
+        this.ctx = ctx;
+    },
 
-  initComponent: function() {
-	$( "#dialog" ).dialog({
-		dialogClass: "no-close",
-	  	position: { my: "left bottom", at: "left bottom", of: window }
-	});
-  }
+    initComponent : function() {
+        $("#dialog").dialog({
+            dialogClass : "no-close",
+            position : {
+                my : "left bottom",
+                at : "left bottom",
+                of : window
+            }
+        });
+    }
 });
 
-//组件盒子，
+// 组件盒子，
 var KwComponentBox = classdef({
-	constructor: function(ctx) {
-	this.initComponent();
-	this.ctx = ctx;
-  },
+    constructor : function(ctx) {
+        this.initComponent();
+        this.ctx = ctx;
+    },
 
-  initComponent: function() {
-	this.dom = $( "#componentList" );
-	this.dom.dialog({
-		dialogClass: "no-close",
-	  	position: { my: "right bottom", at: "right bottom", of: window },
-		width : 405,
-		height : 450
-	});
+    initComponent : function() {
+        this.dom = $("#componentList");
+        this.dom.dialog({
+            dialogClass : "no-close",
+            position : {
+                my : "right bottom",
+                at : "right bottom",
+                of : window
+            },
+            width : 405,
+            height : 450
+        });
 
-	util.registerNotDrop(this.dom, 'cmpBox');
+        util.registerNotDrop(this.dom, 'cmpBox');
 
-	this.loadList("");
+        this.loadList("");
 
-  },
+    },
 
-	loadList : function(lastCmpId){
-		var self = this;
-		$.get('/cmpList/' + lastCmpId, function(ret){
-			self.initList(ret);
-		});
-	},
+    loadList : function(lastCmpId) {
+        var self = this;
+        $.get('/cmpList/' + lastCmpId, function(ret) {
+            self.initList(ret);
+        });
+    },
 
-	initList : function(ret){
-		if(ret.data){
-			var html = '';
-			for(var i in ret.data){
-				var item = ret.data[i];
-				html += '<div class="cmp-define">';
-				html += item.title;
-				html += '<textarea style="display:none;" >' + JSON.stringify(item) + '</textarea>';
-				html += '</div>';
-			}
+    initList : function(ret) {
+        if (ret.data) {
+            var html = '';
+            for ( var i in ret.data) {
+                var item = ret.data[i];
+                html += '<div class="cmp-define">';
+                html += item.title;
+                html += '<textarea style="display:none;" >' + JSON.stringify(item) + '</textarea>';
+                html += '</div>';
+            }
 
-			this.dom.find('.list').append(html);
-			this.dom.find('.cmp-define').draggable({
-				cursor: 'move',
-				cursorAt: { top: 3, left: 3 } ,
-				revert: "invalid",
-				helper: function(){
-					return '<div style="width:18px;height:18px;background-color:#333;"></div>';
-				},
-				appendTo : 'body',
-				zIndex: 1000,
-				start : function(){
-					dropUtil.start();
-				}
-			});
-		}
-	}
+            this.dom.find('.list').append(html);
+            this.dom.find('.cmp-define').kwdraggable();
+        }
+    }
 });
 
-$(document).ready(function(){
-	var ctx = new KwContext();
-	var box = new KwTreeBox(ctx);
-	var cmpList = new KwComponentBox(ctx);
+$(document).ready(function() {
+    var ctx = new KwContext();
+    var box = new KwTreeBox(ctx);
+    var cmpList = new KwComponentBox(ctx);
 
-	ctx.initRootCmp({
-		artifactId  : 'pcBody',
-		groupId : 'system',
-		title : 'pcBody',
-		leaf : false,
-		category : 'system',
-		thumbnail : '',
-		attrs : [],
-		events : []
-	});
+    ctx.initRootCmp({
+        artifactId : 'pcBody',
+        groupId : 'system',
+        title : 'pcBody',
+        leaf : false,
+        category : 'system',
+        thumbnail : '',
+        attrs : [],
+        events : []
+    });
 
 });
--- web/static/js/kiwidragdrop.js
+++ web/static/js/kiwidragdrop.js
@@ -5,134 +5,171 @@ var kwdd = {
 
     // 在拖动时，焦点跟着移动
     docmousemove : function(e) {
-        $('#ddhandler').css({
-            top : e.pageY - 10,
-            left : e.pageX - 10
+        kwdd.movecursor({
+            x : e.pageX,
+            y : e.pageY
         });
     },
 
     // 在document上放，说明没有接收者
     docmouseup : function(e) {
+        console.debug('up document');
         // 清除事件
         kwdd.docclearup();
     },
 
     // 清除document上的事件
     docclearup : function() {
-        
+
         $(document).off('mousemove', kwdd.docmousemove);
         $(document).off('mouseup', kwdd.docmouseup);
-        
-        $('#ddhandler').css({
-            'display' : 'none'
-        });
-        
-        $('.kw-droppable').trigger('stopDrag', [kwdd.target]);
+
+        kwdd.hidecursor();
+
+        $('.kw-droppable').trigger('stopDrag', [ kwdd.target ]);
 
         kwdd.target = null;
+    },
+
+    appendcursor : function() {
+        var html = '<div id="ddhandlertop" class="ddhandler" style="z-index:99999;border-top:2px dotted #000000;width:14px;position:absolute;"></div>';
+        html += '<div id="ddhandlerbottom" class="ddhandler" style="z-index:99999;border-bottom:2px dotted #000000;width:14px;position:absolute;"></div>';
+        html += '<div id="ddhandlerleft" class="ddhandler" style="z-index:99999;border-left:2px dotted #000000;height:14px;position:absolute;"></div>';
+        html += '<div id="ddhandlerright" class="ddhandler" style="z-index:99999;border-right:2px dotted #000000;height:14px;position:absolute;"></div>';
+        $('body').append(html);
+    },
+
+    showcursor : function() {
+        $('.ddhandler').css('display', 'block');
+    },
+
+    hidecursor : function() {
+        $('.ddhandler').css('display', 'none');
+    },
+
+    movecursor : function(point) {
+        $('#ddhandlertop').css({
+            top : point.y - 10,
+            left : point.x - 6
+        });
+        $('#ddhandlerbottom').css({
+            top : point.y + 10,
+            left : point.x - 6
+        });
+        $('#ddhandlerleft').css({
+            top : point.y - 7,
+            left : point.x - 10
+        });
+        $('#ddhandlerright').css({
+            top : point.y - 7,
+            left : point.x + 10
+        });
+
+        // 取消文本选定
+        if (document.selection) {
+            document.selection.empty();
+        } else if (window.getSelection) {
+            window.getSelection().removeAllRanges();
+        }
     }
 };
 
 $.fn.extend({
-    
+
     // 拖
     kwdraggable : function(obj) {
-        
+
         obj = $.extend({}, obj);
-        
+
         $(this).mousedown(function(e) {
-            
+
             // 停止冒泡，不能同时拖动多个
             e.stopPropagation();
-            
-            var csobj = {
-                top : e.pageY - 10,
-                left : e.pageX - 10
-            };
 
-            if ($('#ddhandler').length == 0) {
-                $('body').append('<div id="ddhandler" style="border:none;border-radius:10px;padding:0px;width:20px;height:20px;background:#8888AA;position:absolute;"></div>');
+            if ($('.ddhandler').length == 0) {
+                kwdd.appendcursor();
             } else {
-                csobj['display'] = 'block';
+                kwdd.showcursor();
             }
 
-            $('#ddhandler').css(csobj);
+            kwdd.movecursor({
+                x : e.pageX,
+                y : e.pageY
+            });
 
             $(document).on('mousemove', kwdd.docmousemove);
             $(document).on('mouseup', kwdd.docmouseup);
 
             kwdd.target = $(this);
-            
-            $('.kw-droppable').trigger('startDrag', [kwdd.target]);
+
+            $('.kw-droppable').trigger('startDrag', [ kwdd.target ]);
         });
     },
     // 放
     kwdroppable : function(obj) {
-        
+
         obj = $.extend({}, obj);
 
-        // 接收哪些元素        
+        // 接收哪些元素
         var isAccept = function(target) {
-            if (undefined === obj.accept){
+            if (undefined === obj.accept) {
                 return true;
             }
-            
-            return target.hasClass(obj.accept);            
+
+            return target.hasClass(obj.accept);
         }
-        
+
         $(this).addClass('kw-droppable');
-        
+
         // 响应开始拖动事件
-        $(this).on('startDrag', function(target){
+        $(this).on('startDrag', function(target) {
             if (kwdd.target && isAccept(kwdd.target)) {
                 if (obj.activate) {
-                    obj.activate({
-                        drager : kwdd.target,
-                        droper : $(this)
+                    obj.activate.call(this, {
+                        drager : kwdd.target
                     });
                 }
             }
         });
-        
+
         // 响应停止拖动事件
-        $(this).on('stopDrag', function(target){
+        $(this).on('stopDrag', function(target) {
             if (kwdd.target && isAccept(kwdd.target)) {
                 if (obj.deactivate) {
-                    obj.deactivate({
-                        drager : kwdd.target,
-                        droper : $(this)
+                    obj.deactivate.call(this, {
+                        drager : kwdd.target
                     });
                 }
             }
         });
-        
+
         // 拖动进入此区域
-        $(this).mouseover(function(e){
+        $(this).mouseover(function(e) {
+            console.debug('over me:' + $(this).attr('id'));
             if (kwdd.target && isAccept(kwdd.target)) {
                 // 停止事件冒泡
                 e.stopPropagation();
 
                 // 触发放事件
                 if (obj.over) {
-                    obj.over({
-                        drager : kwdd.target,
-                        droper : $(this)
+                    obj.over.call(this, {
+                        drager : kwdd.target
                     });
                 }
             }
         });
-        
+
         // 拖动离开此区域
-        $(this).mouseout(function(e){
+        $(this).mouseout(function(e) {
+            console.debug('out me:' + $(this).attr('id'));
             if (kwdd.target && isAccept(kwdd.target)) {
                 // 停止事件冒泡
                 e.stopPropagation();
 
                 // 触发放事件
                 if (obj.out) {
-                    obj.out({
-                        drager : kwdd.target,
-                        droper : $(this)
+                    obj.out.call(this, {
+                        drager : kwdd.target
                     });
                 }
             }
@@ -140,15 +177,15 @@ $.fn.extend({
 
         // 放事件发生
         $(this).mouseup(function(e) {
+            console.debug('up me:' + $(this).attr('id'));
             if (kwdd.target && isAccept(kwdd.target)) {
                 // 停止事件冒泡
                 e.stopPropagation();
 
                 // 触发放事件
                 if (obj.drop) {
-                    obj.drop({
-                        drager : kwdd.target,
-                        droper : $(this)
+                    obj.drop.call(this, {
+                        drager : kwdd.target
                     });
                 }
 
